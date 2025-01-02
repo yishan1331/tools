6b49a9100e23dd26062af1f6ad55358351195932
yishan1331 <62239601+yishan1331@users.noreply.github.com>
2025-01-02 22:44:24 +0800

    update check_celery_status.sh

--- sh/check_celery_status.sh
+++ sh/check_celery_status.sh
@@ -3,7 +3,6 @@ export PATH=/bin:/sbin:/usr/bin:/usr/sbin
 SERVICE="celery"
 if ps ax | grep -v grep | grep $SERVICE > /dev/null
 then
-    #echo "$SERVICE service running, everything is fine"  | mail -s "測試信" yishanjob13@sapido.com.tw 
     #echo "$SERVICE service running, everything is fine"  | mail -s "$SERVICE is not running" yishanjob13@gmail.com
 else
     #echo "$SERVICE is not running"
